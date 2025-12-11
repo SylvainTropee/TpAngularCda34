23e59c803a8a291a02c157daf1c58727d66dac06
stropee <stropee@campus-eni.fr>
2025-12-11 12:30:07 +0100

    TP 6

--- src/app/components/carbon-footprint-form/carbon-footprint-form.ts
+++ src/app/components/carbon-footprint-form/carbon-footprint-form.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {CarbonFootprintCompute} from '../../services/carbon-footprint-compute';
 
 @Component({
   selector: 'app-carbon-footprint-form',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './carbon-footprint-form.html',
   styleUrl: './carbon-footprint-form.css',
   standalone: true
 })
 export class CarbonFootprintForm {
 
+  public travelForm: FormGroup = new FormGroup({})
+
+  constructor(private cfpc: CarbonFootprintCompute) {
+    this.travelForm = new FormGroup(
+      {
+        travelType: new FormControl('car', [Validators.pattern(/(car|train|plane)/), (control) => this.travelTypeValidator(control)]),
+        distance: new FormControl(null, [Validators.min(1), Validators.required]),
+        consumption: new FormControl(0, [Validators.min(1), Validators.required]),
+        date: new FormControl(null, [Validators.required])
+      }
+    )
+  }
+
+  travelTypeValidator(control: AbstractControl) {
+
+    const travelType = control.value
+
+    if (travelType == 'car') {
+      this.travelForm.get('consumption')?.setValidators([Validators.min(1), Validators.required])
+    } else {
+      this.travelForm.get('consumption')?.clearValidators()
+      this.travelForm.get('consumption')?.setValue(0)
+    }
+
+    //restester la valeur et déclenché la validation
+    this.travelForm.get("consumption")?.updateValueAndValidity()
+    return null
+  }
+
+
+  onTravelSubmit() {
+    console.log(this.travelForm.valid)
+
+    if (this.travelForm.valid) {
+      let value = this.travelForm.value
+      if (value.distance && value.consumption) {
+        let quantityCo2 = this.cfpc.quantityCo2ByTravel(value.distance, value.consumption)
+        this.cfpc.addTravel(
+          {distance: value.distance, consumptionPer100Km: value.consumption, quantityCo2: quantityCo2}
+        )
+      }
+    }
+  }
 }
--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,24 +1,44 @@
 import {AfterContentChecked, Component} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {User} from '../../services/user';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './home.html',
   styleUrl: './home.css',
   standalone: true
 })
-export class Home implements AfterContentChecked{
+export class Home implements AfterContentChecked {
 
   error?: string | null
+  userLogin: string = ""
+  password: string = ""
+  errors: string[] = []
 
   constructor(private router: Router, private authService: User, private route: ActivatedRoute) {
   }
 
   login() {
-    this.authService.login("Michel")
-    this.goToSummary()
+
+    this.errors = []
+
+    if (this.userLogin.length < 3) {
+      this.errors.push("Le login doit avoir au moins 3 caractères")
+    }
+
+    console.log(this.password)
+    if (this.password.length < 6) {
+      this.errors.push("Le mot de passe doit avoir au moins  6 caractères")
+    }
+
+    if (this.errors.length == 0) {
+      this.authService.login(this.userLogin)
+      this.goToSummary()
+    }
   }
 
   goToSummary() {
